1e2d39152db7fbf888735124a3f237c9071c0906
Gbenga Opeyemi <opesam42@gmail.com>
2025-04-14 22:29:47 +0100

    add sttic files

--- skedlease/settings.py
+++ skedlease/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Ensures Django can find static files
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
