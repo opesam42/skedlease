5d49ed93d8e08a8bb1d5cc5e53098e76b2dfcdfc
Gbenga Opeyemi <opesam42@gmail.com>
2025-05-02 11:06:51 +0100

    complete basic appointments view endpoint

--- core/views.py
+++ core/views.py
@@ -22,9 +22,10 @@ def get_doctors(request):
         doctors = doctors.filter(query)
     
     if not doctors.exists():
-        return Response({
-            'message': 'No doctor found for this speciality'
-        })
+        return Response(
+            {'message': 'No doctor found for this speciality'},
+            status=status.HTTP_404_NOT_FOUND
+        )
 
     serializers = DoctorSerializer(doctors, many=True)
     return Response(serializers.data, status=status.HTTP_200_OK)
@@ -33,5 +34,22 @@ def get_doctors(request):
 @api_view(['GET'])
 def view_appointments(request):
     appointments = Appointment.objects.all()
+
+    patient = request.query_params.get('patient', None)
+    doctor = request.query_params.get('doctor', None)
+
+    if patient:
+        query = Q(patient__id = int(patient))
+        appointments = appointments.filter(query)
+    if doctor:
+        query = Q(doctor__id = int(doctor))
+        appointments = appointments.filter(query)
+
+    if not appointments.exists():
+        return Response(
+            {'message': 'No appointments found'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
     serializers = AppointmentSerializer(appointments, many=True)
     return Response(serializers.data, status=status.HTTP_200_OK)
